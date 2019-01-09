74762edc9f7dc5064c554eafd71ddd5cb32f1060
FSDonnelly <donnelly_frank@yahoo.com>
2019-01-09 14:22:02 -0800

    added auth capability for sign in and login

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,48 +1,41 @@
 var db = require("../models");
-var express = require("express");
-var app = express();
-var path = require("path");
-// module.exports = function(app) {
-  // Load index page
-  // app.get("/", function(req, res) {
-  //   db.Example.findAll({}).then(function(dbExamples) {
-  //     res.render("index", {
-  //       msg: "Welcome!",
-  //       examples: dbExamples
-  //     });
-  //   });
-  // });
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+module.exports = function(app) {
+  // Load signup page
+  app.get("/", function(req, res) {
+    return res.render("signup");
+  });
+
+  // Load login page
+  app.get("/login", function(req, res) {
+    res.render("login");
+  });
+
+  // Load profile page
+  app.get("/profile", isAuthenticated, function(req, res) {
+    db.User.findOne({
+      where: {
+        id: req.user.id
+      },
+      include: [db.Example]
+    }).then(function(dbUser) {
+      res.render("profile", { user: dbUser });
+    });
+  });
 
   // Load example page and pass in an example by id
-  // app.get("/example/:id", function(req, res) {
-  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(
-  //     dbExample
-  //   ) {
-  //     res.render("example", {
-  //       example: dbExample
-  //     });
-  //   });
-  // });
-  module.exports = function(app) {
+  app.get("/example/:id", isAuthenticated, function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
 
-    // HTML GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases the user is shown an HTML page of content
-    // ---------------------------------------------------------------------------
-  
-    // If no matching route is found default to home
-    app.get("/", function(req, res) {
-      res.sendFile(path.join(__dirname, "/../starter.html"));
-    });
-    app.get("/survey", function(req, res) {
-      res.sendFile(path.join(__dirname, "/survey.html"));
-    });
-  
-  
-  };
-  
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
   });
-
+};
