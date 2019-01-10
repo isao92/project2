69a5f9731935068827eefd87c1c1b46d95256275
FSDonnelly <donnelly_frank@yahoo.com>
2019-01-09 21:53:48 -0800

    able to get authentic api key from .env file now

--- routes/authentic.js
+++ routes/authentic.js
@@ -3,16 +3,17 @@ require("dotenv").config();
 var axios = require("axios");
 var express = require("express");
 var router = express.Router();
-
+console.log(process.env.AUTHENTIC_KEY)
 router.get("/api/jobs", function (req, res) {
-  const key = process.env.AUTHENTIC_KEY
-  axios.get(`https://authenticjobs.com/api/?api_key=${key}=aj.categories.getList&format=json&method=aj.jobs.search&perpage10`).then(
+  const key = process.env.AUTHENTIC_KEY;
+ 
+  axios.get(`https://authenticjobs.com/api/?api_key=${key}&category=aj.categories.getList&format=json&method=aj.jobs.search&perpage10`).then(
     function (response) {
       // console.log(JSON.stringify(response.data));
       res.json(response.data);
     }
-    // ).catch(function(err){
-    //  if (err)throw err }
+    ).catch(function(err){
+     if (err)throw err }
   );
 });
 
