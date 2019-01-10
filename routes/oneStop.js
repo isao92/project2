10fd58f2c74450c49224e1acd7c7ecb62ec6db98
FSDonnelly <donnelly_frank@yahoo.com>
2019-01-09 17:16:53 -0800

    created .env file for api keys and npm installed dotenv package

--- routes/oneStop.js
+++ routes/oneStop.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
 // Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
 var axios = require("axios");
 var express = require("express");
 var router = express.Router();
 
 router.get("/api/careers", function (req, res) {
-  const token = "3eLG7hgV7sgwNHpdSAfJYD6OxnbAZl56ZeJDYYMbB2YzOwW/F+TRucZkejBgc1hCVt6SOvWY4OiA3CihuzarKA==";
-  axios.get("https://api.careeronestop.org/v1/jobsearch/tNUi5ft3dAjirVq/public%20relations/CA/25/0/0/0/10/30?source=NLx&showFilters=false", {
+  const token = process.env.CAREER_TOKEN;
+  const id = process.env.CAREER_ID;
+  axios.get(`https://api.careeronestop.org/v1/jobsearch/${id}/public%20relations/CA/25/0/0/0/10/30?source=NLx&showFilters=false`, {
     headers: {
       Authorization: 'Bearer ' + token //the token is a variable which holds the token
     }
