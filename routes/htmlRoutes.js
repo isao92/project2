1ed382e4c838ab31f6dc57f0c26d180e45fffb19
isao92 <01agustin92@gmail.com>
2019-01-12 21:51:23 -0800

    New logo looking sexy

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,24 +4,46 @@ var db = require("../models");
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 var axios = require("axios");
 
+var findExp = [];
+var x = 0;
+var hasLoaded = false;
 
-
-module.exports = function(app) {
+module.exports = function (app) {
   // Load signup page
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     return res.render("signup");
   });
 
   // Load login page
-  app.get("/login", function(req, res) {
+  app.get("/login", function (req, res) {
     res.render("login");
   });
 
-  app.get("/results/:keyword/:state", function(req, res) {
-    console.log("Wohooo!");
-  
+  app.get("/results/:keyword/:state/:experience", function (req, res) {
+
     var state = req.params.state;
     var keyword = req.params.keyword;
+    var experience = req.params.experience;
+
+    // set keywords according to experience level
+    if (experience == "lessThanThree") {
+      findExp = ["Internship", "Jr", "Volunteer", "first-level", "Associate's"];
+      console.log("lessThanThree");
+      hasLoaded = true;
+
+    } else if (experience == "threeToFive") {
+      findExp = ["Bachelor's", "bachelor's", "intermediate", "middle-level", "awareness", "Four years"];
+      console.log("threeToFive");
+      hasLoaded = true;
+
+    } else if (experience == "moreThanFive") {
+      findExp = ["Senior", "Supervisor", "Excecutive", "top-level", "higher grade level", "Master's"];
+      console.log("moreThanFive");
+      hasLoaded = true;
+
+    }
+
+
     // console.log(keyword);
     var host = 'data.usajobs.gov';
     var userAgent = 'taylor.walker@hotmail.com';
@@ -34,45 +56,93 @@ module.exports = function(app) {
       }
     }).then(
       function (response) {
-        console.log(response.data);
+
+        // Loop goes through all job postings
+        for (var j = 0; j < response.data.SearchResult.SearchResultItems.length; j++) {
+          
+          // get id of current job posting being analyzed
+          console.log(response.data.SearchResult.SearchResultItems[j].MatchedObjectId);
+          
+          // Loop goes through each keyword from experience level
+          for (var i = 0; i < findExp.length; i++) {
+
+            // hasLoaded becomes true when experience level is detected
+            if (hasLoaded) {
+              try {
+                // store qualification summary into variable
+                var quaSumm = response.data.SearchResult.SearchResultItems[j].MatchedObjectDescriptor.QualificationSummary;
+                // store boolean in variable if keyword is detected
+                var foundExpLevel = quaSumm.includes(findExp[i]);
+                
+                // find keyword related to the experience level
+                if (foundExpLevel) {
+                  console.log("Found a match!");
+
+                  // get unique id of matched event and store it in a variable
+                  var matchedId = response.data.SearchResult.SearchResultItems[j].MatchedObjectId;
+
+                  console.log("This is the matched job posting: " + matchedId);
+
+                  var theMatchedExpLvl = [];
+                  
+                  theMatchedExpLvl[x]= quaSumm;
+                  // console log the qualification summary of the matched exp level
+                  // console.log(theMatchedExpLvl[x]); 
+                  x++;
+                }  
+
+              
+
+                
+
+              } catch (error) {
+                console.log(error);
+              }
+
+            } else {
+              console.log("No experience level detected");
+            }
+          }
+        }
+        // console.log(response.data);
         // res.json(response.data.SearchResult.SearchResultItems);
         res.render("results", {
           jobs: response.data.SearchResult.SearchResultItems
         });
       }
     );
-    
+
   });
 
 
-   // Load survey page
-   app.get("/survey", isAuthenticated, function(req, res) {
+  // Load survey page
+  app.get("/survey", isAuthenticated, function (req, res) {
     db.User.findOne({
       where: {
         id: req.user.id
       },
       include: [db.Example]
-    }).then(function(dbUser) {
+    }).then(function (dbUser) {
       res.render("survey", { user: dbUser });
     });
   });
-  
+
   // Load profile page
-  app.get("/profile", isAuthenticated, function(req, res) {
+  app.get("/profile", isAuthenticated, function (req, res) {
     db.User.findOne({
       where: {
         id: req.user.id
       },
       include: [db.Example]
-    }).then(function(dbUser) {
+    }).then(function (dbUser) {
       res.render("profile", { user: dbUser });
     });
   });
 
-  
+
   // Load example page and pass in an example by id
-  app.get("/example/:id", isAuthenticated, function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+  app.get("/example/:id", isAuthenticated, function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (
       dbExample
     ) {
       res.render("example", {
@@ -82,7 +152,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
